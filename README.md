4117ae6c5948d2c666d48e6460d8cd9308ce4c6b
Ehsanul Hoque <ehsanul@ehsanul.com>
2023-04-13 01:35:09 -0400

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # focus-flow
+
 A ChatGTP authored app to help you focus on the right things
+
+## Initial Prompt (Note: much followup was required)
+
+Create an app that helps the user make sure that they are spending their time
+as they intend during their work hours, i.e. that they are working on the right
+things. This should be a single page web application written with vanilla JS,
+HTML and CSS, and it should use localstorage to store all state. Do NOT use
+a backend of any kind, this is a pure client-side application.
+
+When the app is initially loaded, the user is greeted with a screen asking them
+what they intend to work on, i.e. their tasks. The user enters their tasks,
+with two fields:
+
+1. Task Name
+2. Estimated time in hours
+
+The user may add as many of these tasks as they like, and can see the entire
+list as they add to it, including the total time for all the tasks. The user
+may also edit any of the task names or time estimates, or even entirely remove
+tasks.
+
+Once the user is happy with this, they should be able to submit the list and
+initialize the task list for the day. Upon submitting, the app should store
+this list in localstorage for later use, and then start a timer, which is
+displayed on the screen counting down.
+
+The timer is for 15 minutes. Every 15 minutes throughout the day, when the
+timer reaches zero, the app should loop an audible beep. At the same time, the
+app displays the list of task names as large clickable buttons, with a question
+asking the user: "What are you doing?". In addition to the task options that
+were previously entered, there should also be an "Other" button, which allows
+a free-text input upon clicking to record whatever the user was actually doing,
+and a submit button for that too. This should be stored in localstorage along
+with a timestamp.
+
+When the task buttons or "Other" button are clicked, the beeping should stop
+immediately and the 15 minute timer should restart. The selection should be
+tracked and shown in a visualization showing how many times each option was
+selected in total, each counting for 15 minutes of time. This visualization
+should have a scale of time in hours. The "Other" option should also be broken
+down into the specific unique items entered by the user for this visualization.
+
+Also support an option to pause the timer, either for 1 hour or until
+a specific time on the next work day, at which point it will beep again to
+remind the user to use the app in the next work day. Also ask the user to edit
+their items and reset their selection stats at the start of each day.
+
+Please provide separate HTML, JS and CSS files for this application. It should
+have a modern style, with a grid layout and plenty of whitespace and great
+typography. Please also make it a dark-mode theme by default, with an option to
+switch to light mode.
+
+Come up with your own catchy name for the app and add this app name as a small
+text logo on the page.
---
+++ script.js
@@ -0,0 +1,235 @@
+
+const container = document.querySelector(".container");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let taskStats = JSON.parse(localStorage.getItem("taskStats")) || {};
+let timer;
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function saveTaskStats() {
+    localStorage.setItem("taskStats", JSON.stringify(taskStats));
+}
+
+function createTaskForm() {
+    const form = document.createElement("form");
+    form.innerHTML = `
+        <input type="text" name="taskName" placeholder="Task Name" required>
+        <input type="number" name="taskHours" placeholder="Hours" step="0.25" min="0" required>
+        <button type="submit">Add Task</button>
+    `;
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        tasks.push({
+            name: formData.get("taskName"),
+            hours: parseFloat(formData.get("taskHours")),
+        });
+        saveTasks();
+        displayTasks();
+        form.reset();
+        form.querySelector('input[name="taskName"]').focus();
+    });
+    container.appendChild(form);
+    displayTasks();
+}
+
+function displayTasks() {
+    const taskList = document.querySelector('.task-list');
+    if (taskList) {
+        container.removeChild(taskList);
+    }
+
+    const newTaskList = document.createElement("ul");
+    newTaskList.classList.add("task-list");
+    newTaskList.innerHTML = tasks
+        .map(
+            (task, index) => `
+        <li>
+            <input type="text" value="${task.name}" data-index="${index}" class="task-name">
+            <input type="number" value="${task.hours}" data-index="${index}" class="task-hours" step="0.25" min="0">
+            <button data-index="${index}" class="remove-task">Remove</button>
+        </li>
+    `
+        )
+        .join("");
+    container.insertBefore(newTaskList, container.querySelector('form'));
+
+    newTaskList.querySelectorAll(".task-name").forEach((input) =>
+        input.addEventListener("input", (e) => {
+            tasks[e.target.dataset.index].name = e.target.value;
+            saveTasks();
+        })
+    );
+
+    newTaskList.querySelectorAll(".task-hours").forEach((input) =>
+        input.addEventListener("input", (e) => {
+            tasks[e.target.dataset.index].hours = parseFloat(e.target.value);
+            saveTasks();
+        })
+    );
+
+    newTaskList.querySelectorAll(".remove-task").forEach((button) =>
+        button.addEventListener("click", (e) => {
+            tasks.splice(e.target.dataset.index, 1);
+            saveTasks();
+            displayTasks();
+        })
+    );
+}
+
+function startTimer() {
+    clearInterval(timer);
+    let timeLeft = 15; // * 60;
+    const timerDisplay = document.createElement("div");
+
+    function updateTimerDisplay() {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        timerDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    }
+
+    function onTimerTick() {
+        timeLeft--;
+        updateTimerDisplay();
+
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            timerDisplay.remove();
+            playBeep();
+            askUserWhatTheyAreDoing();
+        }
+    }
+
+    updateTimerDisplay();
+    container.appendChild(timerDisplay);
+    timer = setInterval(onTimerTick, 1000);
+}
+
+let beepAudio;
+function playBeep() {
+    beepAudio = new Audio(
+        "https://freesound.org/data/previews/316/316847_4939433-lq.mp3"
+    );
+    beepAudio.loop = true;
+    beepAudio.play();
+}
+
+function stopBeep() {
+    if (beepAudio) {
+        beepAudio.pause();
+        beepAudio.currentTime = 0;
+    }
+}
+
+function askUserWhatTheyAreDoing() {
+    const additionalTasks = [
+        { name: "Meeting" },
+        { name: "Slack" },
+    ];
+
+    const uniqueTasks = tasks.concat(
+        additionalTasks.filter(
+            (additionalTask) =>
+                !tasks.some(
+                    (task) =>
+                        task.name.toLowerCase() ===
+                        additionalTask.name.toLowerCase()
+                )
+        )
+    );
+
+
+    const prompt = document.createElement("div");
+    prompt.innerHTML = `
+        <h2>What are you doing?</h2>
+        ${uniqueTasks
+            .map(
+                (task, index) => `
+            <button data-index="${index}" class="task-button">${task.name}</button>
+        `
+            )
+            .join("")}
+        <button id="other-button">Other</button>
+        <form id="other-form" style="display:none;">
+            <input type="text" name="otherTask" placeholder="Other task" required>
+            <button type="submit">Submit</button>
+        </form>
+    `;
+    container.appendChild(prompt);
+
+    const handleButtonClick = () => {
+        stopBeep();
+        container.removeChild(prompt);
+        startTimer();
+    };
+
+    prompt.querySelectorAll(".task-button").forEach((button) =>
+        button.addEventListener("click", (e) => {
+            const taskIndex = e.target.dataset.index;
+            taskStats[uniqueTasks[taskIndex].name] =
+                (taskStats[uniqueTasks[taskIndex].name] || 0) + 1;
+            saveTaskStats();
+            displayTaskStats();
+            handleButtonClick();
+        })
+    );
+
+    const otherButton = document.getElementById("other-button");
+    const otherForm = document.getElementById("other-form");
+
+    otherButton.addEventListener("click", () => {
+        otherButton.style.display = "none";
+        otherForm.style.display = "block";
+        if (beepAudio) {
+          beepAudio.volume = 0.25; // make it quieter while typing
+        }
+    });
+
+    otherForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(otherForm);
+        const otherTask = formData.get("otherTask");
+        taskStats[otherTask] = (taskStats[otherTask] || 0) + 1;
+        saveTaskStats();
+        displayTaskStats();
+        handleButtonClick();
+    });
+}
+
+
+function displayTaskStats() {
+    const taskStatsList = document.createElement("ul");
+    taskStatsList.innerHTML = Object.entries(taskStats)
+        .map(
+            ([task, count]) => `
+        <li>
+            ${task}: ${count * 15 / 60} hours
+        </li>
+    `
+        )
+        .join("");
+    container.appendChild(taskStatsList);
+}
+
+function createStartButton() {
+    const startButton = document.createElement("button");
+    startButton.textContent = "Start Timer";
+    startButton.addEventListener("click", () => {
+        startTimer();
+        startButton.remove();
+    });
+    container.appendChild(startButton);
+}
+
+createTaskForm();
+displayTasks();
+createStartButton();
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (tasks.length > 0) {
+        startTimer();
+    }
+});
